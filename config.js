bd6c778e61ce294d4aebc54c1e6ccf6cf4197335
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-26 00:22:16 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://hnymqvkfmythdxtjqeam.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhueW1xdmtmbXl0aGR4dGpxZWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMjE3NTUsImV4cCI6MjA3ODY5Nzc1NX0.4ww5fKNnbhNzNrkJZLa466b6BsKE_yYMO2YH6-auFlI',
+    OWNER_UID: '767e5f7d-0c7a-4c32-80e3-b9613e0d37df',
+    TELEGRAM_BOT_TOKEN: '', // Add your Telegram bot token here
+    TELEGRAM_CHAT_ID: '' // Add your Telegram chat ID here
+};
